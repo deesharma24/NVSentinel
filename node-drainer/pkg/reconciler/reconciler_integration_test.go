9b44e0dd8d95d9a5ab2cbca731ca907b8d66eee4
Deepak Sharma <deesharma@nvidia.com>
2026-01-14 10:32:51 +0530

    feat: add support to pass  appname to mongodb to track clients

--- node-drainer/pkg/reconciler/reconciler_integration_test.go
+++ node-drainer/pkg/reconciler/reconciler_integration_test.go
@@ -135,6 +135,10 @@ func (m *mockDatabaseConfig) GetTimeoutConfig() sdkconfig.TimeoutConfig {
 	return &mockTimeoutConfig{}
 }
 
+func (m *mockDatabaseConfig) GetAppName() string {
+	return "node-drainer"
+}
+
 // mockCertConfig is a simple mock implementation for testing
 type mockCertConfig struct{}
 
